feat: fixing attention mask again

hbgl/criterion.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import BCEWithLogitsLoss
+from torch.nn import BCELoss
 
 from hbgl.config import HBGLConfig
 
@@ -10,7 +10,7 @@ class HBGLLoss:
     ):
         self._label_token_indices = [i for i in range(config.label_count)]
         self._label_level_indices = self._prepare_label_level_indices(config)
-        self._bce = BCEWithLogitsLoss(reduction="sum")
+        self._bce = BCELoss(reduction="sum")
     
     def _prepare_label_level_indices(
         self,

hbgl/data_collator.py
@@ -2,6 +2,12 @@ import torch
 from transformers import BatchEncoding
 
 from hbgl.config import HBGLConfig
+from hbgl.utils import (
+    prepare_mask_inputs_from_label,
+    prepare_token_type_ids_from_input_ids,
+    prepare_position_ids_from_input_ids,
+    prepare_4d_attention_mask,
+)
 
 class HBGLDataCollator:
     def __init__(self, config: HBGLConfig):
@@ -57,4 +63,39 @@ class HBGLDataCollator:
             new_inputs[a] = torch.tensor(inputs[a])
         new_inputs[LABEL_ATTRIBUTE] = new_inputs[LABEL_ATTRIBUTE].float()
 
-        return BatchEncoding(new_inputs)
+        text_input_ids = new_inputs["input_ids"]
+        label_input_ids = new_inputs["label_input_ids"]
+
+        label_attention_mask = new_inputs["label_attention_mask"]
+
+        mask_input_ids, mask_attention_mask = prepare_mask_inputs_from_label(
+            label_input_ids=label_input_ids,
+            label_attention_mask=label_attention_mask,
+            mask_token_id=self._mask_token_id
+        )
+        token_type_ids = prepare_token_type_ids_from_input_ids(
+            text_input_ids=text_input_ids,
+            label_input_ids=label_input_ids,
+            mask_input_ids=mask_input_ids,
+        )
+        position_ids = prepare_position_ids_from_input_ids(
+            text_input_ids=new_inputs["input_ids"],
+            label_input_ids=new_inputs["label_input_ids"],
+            mask_input_ids=mask_input_ids,
+        )
+        attention_mask = prepare_4d_attention_mask(
+            text_attention_mask_2d=new_inputs["attention_mask"],
+            label_attention_mask_2d=new_inputs["label_attention_mask"],
+            mask_attention_mask_2d=mask_attention_mask,
+        )
+        labels = new_inputs[LABEL_ATTRIBUTE]
+
+        return BatchEncoding({
+            "text_input_ids": text_input_ids,
+            "label_input_ids": label_input_ids,
+            "mask_input_ids": mask_input_ids,
+            "token_type_ids": token_type_ids,
+            "position_ids": position_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
+        })

hbgl/modeling.py
@@ -40,26 +40,23 @@ class HBGLModel(nn.Module):
         return embeddings
 
     def forward(
-            self,
-            input_ids: torch.Tensor,
-            attention_mask: torch.Tensor,
-            label_input_ids: torch.Tensor,
-            label_attention_mask: torch.Tensor,
+        self,
+        text_input_ids: torch.Tensor,
+        label_input_ids: torch.Tensor,
+        mask_input_ids: torch.Tensor,
+        attention_mask: torch.Tensor,
+        token_type_ids: torch.Tensor,
+        position_ids: torch.Tensor,
     ):
-        input_ids = input_ids.to(self._device)
-        attention_mask = attention_mask.to(self._device)
+        text_input_ids = text_input_ids.to(self._device)
         label_input_ids = label_input_ids.to(self._device)
-        label_attention_mask = label_attention_mask.to(self._device)
-
-        mask_input_ids, mask_attention_mask = self._prepare_mask_inputs(label_input_ids, label_attention_mask)
+        mask_input_ids = mask_input_ids.to(self._device)
+        attention_mask = attention_mask.to(self._device)
+        token_type_ids = token_type_ids.to(self._device)
+        position_ids = position_ids.to(self._device)
 
         # Harusnya nanti di sini pakai input_embeds karena kita pakai label embeddings yang tidak ada dalam token.
-        inputs_embeds = self._prepare_inputs_embeds(input_ids, label_input_ids, mask_input_ids)
-        token_type_ids = self._prepare_token_type_ids(input_ids, label_input_ids, mask_input_ids)
-        position_ids = self._prepare_position_ids(input_ids, label_input_ids, mask_input_ids)
-
-        attention_mask = self._prepare_attention_mask(attention_mask, label_attention_mask, mask_attention_mask)        
-
+        inputs_embeds = self._prepare_inputs_embeds(text_input_ids, label_input_ids, mask_input_ids)
         outputs = self.bert(
             inputs_embeds=inputs_embeds,
             token_type_ids=token_type_ids,
@@ -71,7 +68,7 @@ class HBGLModel(nn.Module):
             raise ValueError("last_hidden_state is not a torch.Tensor")
         
         # Ini ukurannya [batch_size, text_len + label_len + mask_len, dimension]
-        last_hidden_state = last_hidden_state[:, -mask_attention_mask.shape[1]:]
+        last_hidden_state = last_hidden_state[:, -mask_input_ids.shape[1]:]
         scores = torch.matmul(last_hidden_state, self.label_embeddings.T)
         scores = torch.sigmoid(scores)
         return scores
@@ -79,15 +76,6 @@ class HBGLModel(nn.Module):
     def _forward_word_embeddings(self, x: torch.Tensor) -> torch.Tensor:
         bert_embeddings: nn.Embedding = self._get_word_embeddings()
         return bert_embeddings(x)
-    
-    def _prepare_mask_inputs(
-        self,
-        label_input_ids: torch.Tensor,
-        label_attention_mask: torch.Tensor,
-    ):
-        input_ids = torch.full_like(label_input_ids, self.config.mask_token_id)
-        attention_mask = label_attention_mask
-        return input_ids, attention_mask
     
     def _prepare_inputs_embeds(
         self,
@@ -111,78 +99,3 @@ class HBGLModel(nn.Module):
             label_embeds,
             mask_embeds,
         ], dim=1)
-    
-    def _prepare_token_type_ids(
-        self,
-        input_ids: torch.Tensor,
-        label_input_ids: torch.Tensor,
-        mask_input_ids: torch.Tensor,
-    ):
-        text_type_ids = torch.zeros_like(input_ids)
-        label_type_ids = torch.ones_like(label_input_ids)
-        mask_type_ids = torch.ones_like(mask_input_ids)
-        return torch.cat([text_type_ids, label_type_ids, mask_type_ids], dim=1)
-    
-    def _prepare_position_ids(
-        self,
-        input_ids: torch.Tensor,
-        label_input_ids: torch.Tensor,
-        mask_input_ids: torch.Tensor,
-    ):
-        text_position_ids = batch_arange_like(input_ids)
-        label_position_ids = batch_arange_like(label_input_ids) + 1
-        mask_position_ids = batch_arange_like(mask_input_ids) + 1
-
-        return torch.cat([text_position_ids, label_position_ids, mask_position_ids], dim=1)
-    
-    def _prepare_attention_mask(
-        self,
-        text_attention_mask_2d: torch.Tensor,
-        label_attention_mask_2d: torch.Tensor,
-        mask_attention_mask_2d: torch.Tensor,
-    ):
-        base_attention_mask = torch.cat([text_attention_mask_2d, label_attention_mask_2d, mask_attention_mask_2d], dim=1)
-        base_attention_mask = base_attention_mask.unsqueeze(1)
-
-        first_attention_mask = base_attention_mask.unsqueeze(3)
-        second_attention_mask = base_attention_mask.unsqueeze(2)
-
-        batch_size = base_attention_mask.shape[0]
-
-        text_length = text_attention_mask_2d.shape[1]
-        label_length = label_attention_mask_2d.shape[1]
-        mask_length = mask_attention_mask_2d.shape[1]
-        length = text_length + label_length + mask_length
-        
-        label_start_index = text_length
-        label_stop_index = label_start_index + label_length
-        mask_start_index = label_stop_index
-        mask_stop_index = mask_start_index + mask_length
-
-        label_base_subattention = torch.ones((label_length, label_length), device=self._device)
-        label_to_label_subattention = torch.tril(label_base_subattention)
-
-        label_to_mask_subattention = torch.tril(label_base_subattention, diagonal=-1)
-        label_to_mask_subattention = label_to_mask_subattention[:mask_length]
-
-        mask_to_mask_subattention = torch.eye(mask_length)
-
-        text_attention = torch.ones((batch_size, 1, length, text_length), device=self._device)
-
-        label_attention = torch.zeros((batch_size, 1, length, label_length), device=self._device)
-        label_attention[:, :, label_start_index:label_stop_index, :] = label_to_label_subattention
-        label_attention[:, :, mask_start_index:mask_stop_index, :] = label_to_mask_subattention
-
-        mask_attention = torch.zeros((batch_size, 1, length, mask_length), device=self._device)
-        mask_attention[:, :, mask_start_index:mask_stop_index, :] = mask_to_mask_subattention
-
-        bool_attention_mask = torch.cat([text_attention, label_attention, mask_attention], dim=3)
-        bool_attention_mask = bool_attention_mask * first_attention_mask * second_attention_mask
-        attention_mask = torch.where(
-            bool_attention_mask.bool(),
-            torch.tensor(0.0, device=bool_attention_mask.device),
-            torch.tensor(float("-inf"), device=bool_attention_mask.device),
-        )
-
-        self._last_attention_mask = attention_mask
-        return attention_mask