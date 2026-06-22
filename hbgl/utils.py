059fafd941dfa0cb5227d577cea47d602e7f18cc
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2026-06-22 22:42:19 +0700

    feat: some functions with size, some fix in prepare_4d_attention_mask

--- hbgl/utils.py
+++ hbgl/utils.py
@@ -65,11 +65,11 @@ def prepare_4d_attention_mask(
     mask_start_index = label_stop_index
     mask_stop_index = mask_start_index + mask_length
 
-    label_base_subattention = torch.ones((label_length, label_length), device=device)
-    label_to_label_subattention = torch.tril(label_base_subattention)
+    label_to_label_subattention = torch.ones((label_length, label_length), device=device)
+    label_to_label_subattention = torch.tril(label_to_label_subattention)
 
-    label_to_mask_subattention = torch.tril(label_base_subattention, diagonal=-1)
-    label_to_mask_subattention = label_to_mask_subattention[:mask_length]
+    label_to_mask_subattention = torch.ones((mask_length, label_length))
+    label_to_mask_subattention = torch.tril(label_to_mask_subattention, diagonal=-1)
 
     mask_to_mask_subattention = torch.eye(mask_length)
 
@@ -91,3 +91,70 @@ def prepare_4d_attention_mask(
         torch.tensor(-1e9, device=bool_attention_mask.device),
     )
     return attention_mask
+
+def prepare_4d_boolean_attention_mask_with_size(
+    batch_size: int,
+    text_length: int,
+    label_length: int,
+    mask_length: int,
+):
+    length = text_length + label_length + mask_length
+    
+    label_start_index = text_length
+    label_stop_index = label_start_index + label_length
+    mask_start_index = label_stop_index
+    mask_stop_index = mask_start_index + mask_length
+
+    label_to_label_subattention = torch.ones((label_length, label_length))
+    label_to_label_subattention = torch.tril(label_to_label_subattention)
+
+    label_to_mask_subattention = torch.ones((mask_length, label_length))
+    label_to_mask_subattention = torch.tril(label_to_mask_subattention, diagonal=-1)
+
+    mask_to_mask_subattention = torch.eye(mask_length)
+
+    text_attention = torch.ones((batch_size, 1, length, text_length))
+
+    label_attention = torch.zeros((batch_size, 1, length, label_length))
+    label_attention[:, :, label_start_index:label_stop_index, :] = label_to_label_subattention
+    
+    label_attention[:, :, mask_start_index:mask_stop_index, :] = label_to_mask_subattention
+
+    mask_attention = torch.zeros((batch_size, 1, length, mask_length))
+    mask_attention[:, :, mask_start_index:mask_stop_index, :] = mask_to_mask_subattention
+
+    bool_attention_mask = torch.cat([text_attention, label_attention, mask_attention], dim=3)
+    return bool_attention_mask
+
+def batch_arange(batch_size: int, length: int):
+    x = torch.arange(length)
+    x = x.repeat(batch_size, 1)
+    return x
+
+def prepare_token_type_ids_from_size(
+    batch_size: int,
+    text_length: int,
+    label_length: int,
+    mask_length: int,
+):
+    text_type_ids = torch.zeros((batch_size, text_length))
+    label_type_ids = torch.ones((batch_size, label_length))
+    mask_type_ids = torch.ones((batch_size, mask_length))
+    
+    result = torch.cat([text_type_ids, label_type_ids, mask_type_ids], dim=1)
+    result = result.long()
+    return result
+
+def prepare_position_ids_from_size(
+    batch_size: int,
+    text_length: int,
+    label_length: int,
+    mask_length: int,
+):
+    text_position_ids = batch_arange(batch_size, text_length)
+    label_position_ids = batch_arange(batch_size, label_length) + 1
+    mask_position_ids = batch_arange(batch_size, mask_length) + 1
+
+    result = torch.cat([text_position_ids, label_position_ids, mask_position_ids], dim=1)
+    result = result.long()
+    return result
