f3dd3c16c1a7e3a785b101a28fc85c3bc23b14d7
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2026-07-29 16:18:21 +0800

    fix: remove args.model_type things again

--- prepare.py
+++ prepare.py
@@ -168,7 +168,7 @@ def get_model_and_tokenizer(args):
     logger.info("Construct model %s" % model_class.MODEL_NAME)
 
     model = model_class.from_pretrained(
-        args.model_name_or_path, config=config, model_type=args.model_type,
+        args.model_name_or_path, config=config,
         reuse_position_embedding=True,
         cache_dir=args.cache_dir if args.cache_dir else None)
 
